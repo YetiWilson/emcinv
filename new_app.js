94e0843a8ad963cbc233c39776c8564559caae56
nctiggy <nctiggy@gmail.com>
2016-04-04 00:17:35 -0700

    Working in the modal for XtremIO

--- new_app.js
+++ new_app.js
@@ -1,20 +1,54 @@
 var app = angular.module('emcinv', ['ui.bootstrap','smart-table']);
 
-app.controller('dropdown', function($scope,$http) {
+app.controller('InstallBaseController', function($scope,$http,$uibModal) {
   $scope.selectedCustomer = "Select Customer";
   $http.get('customers.json').success(function(data) {
     $scope.customerList = data;
   });
 
-  $scope.getInventory = function(selectedCustomer) {
-    $scope.selectedCustomer = selectedCustomer.name;
-  }
+  $scope.setIB = function(customer) {
+    $scope.selectedCustomer = customer.name;
+    $http.get('http://pnwreport.bellevuelab.isus.emc.com/api/installs/' + customer.id)
+      .success(function(data) {
+        $scope.safeInstallData = data['rows'];
+      });
+  };
+
+  $scope.modelName = function(model) {
+    switch(model) {
+          case "SD-3D":
+            return "VMAX 40K";
+            break;
+          case "SB-3D":
+            return "VMAX (Orig)";
+            break;
+          case "BA-SYS1E":
+            return "VMAX 10K";
+            break;
+          case "S2-3D":
+            return "VMAX 20K";
+            break;
+          default:
+            return model;
+    }
+  };
+
+  $scope.open = function(sn) {
+    var xtremioModal = $uibModal.open({
+      animation: false,
+      templateUrl: 'xtremioModalContent.html',
+      controller: 'xtremioModalController',
+      resolve: {
+        sn: function() { return sn; }
+      }
+    });
+  };
+
 });
 
-app.controller('table', function($scope) {
-  $scope.safeInstallData = GetInstallBase.getInstallBase();
+app.controller('xtremioModalController', function($scope,$http,$uibModalInstance,sn) {
+  $http.get('http://pnwreport.bellevuelab.isus.emc.com/api/xtremio/' + sn)
+    .success(function(data) {
+      $scope.xtremioData = data['AllObjects'];
+    });
 });
-
-
-app.factory("GetInstallBase", function(
-
